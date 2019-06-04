b10c18b47457d4e6008e66465ae082bed4c24b5e
root <root@bc.xlife.top>
2019-06-04 23:37:34 +0800

    优化充值db脚本

--- script/resetdb.sh
+++ script/resetdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #mongodb 进入client 并use admin
-MongoDB='mongo -u admin -p 111111 --authenticationDatabase=admin'
+MongoDB='mongo amdin' # -u admin -p Xlife.toP201956125001 --authenticationDatabase=admin'
 
 $MongoDB <<EOF
 use explorerDB
@@ -9,10 +9,10 @@ exit;
 EOF
 exit 0
 
-MongoDB='mongo -u admin -p 111111 --authenticationDatabase=admin'
+MongoDB='mongo -u admin -p Xlife.toP201956125001 --authenticationDatabase=admin'
 
 $MongoDB <<EOF
 use explorerDB
-db.createUser( { user: "explorer", pwd: "11111", roles: ["dbOwner"] } )
+db.createUser( { user: "explorer", pwd: "Xlife.toP201956125001", roles: ["dbOwner"] } )
 exit;
 EOF
